7cbb1ea349810ddd21af8e312d70c1862f70923b
Yaniv Vakrat <yavakrat@elta.co.il>
2015-11-22 19:04:33 +0200

    adding support for push

--- src/il/co/ashdod10/Ashdod10.java
+++ src/il/co/ashdod10/Ashdod10.java
@@ -9,8 +9,10 @@ import com.codename1.ui.Dialog;
 import com.codename1.ui.Display;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
+import com.codename1.ui.TextArea;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.util.Resources;
@@ -25,6 +27,7 @@ public class Ashdod10 implements PushCallback{
     //Server side Push API Key - AIzaSyAEEqlL2T1UfpDmtmpw72Ii1ZZJL543TEs
     
     private String GOOGLE_MESSAGING_API_KEY = "926251658740";//Ashdod10 - Google project
+    private static String deviceId1 = "cn1-gcm-APA91bHcn7kdt5DsmXFAP_uj3liLL61ECy7uwHcCAx1Q600Fya1l7T7C6j2of6rDUzKbKo-Ngyq6ZciHdaQUTQjt8n6dgdUh5TGJOC49AY4LxDr4uBJDeznfqmfiqY6rDCtNqKXLULxZ";
 //"876789427701";//12 digits taken from Google messaging
 
     private Form current;
@@ -51,6 +54,8 @@ public class Ashdod10 implements PushCallback{
         });*/
     }
     
+//    TextArea ta;
+    
     public void start()   {        
         try{
             if(current != null){
@@ -59,7 +64,7 @@ public class Ashdod10 implements PushCallback{
             }
             
             AppStructureParser.getInstance();
-            
+                                   
             SplashScreenForm.showSplashScreen();
             
         }catch (Exception e){
@@ -92,7 +97,8 @@ public class Ashdod10 implements PushCallback{
 
     public void registeredForPush(String deviceId) {
         String pushKey = Push.getPushKey();        
-        Dialog.show("registeredForPush", "DeviceId: " + pushKey, "OK", "Cancel");
+        //ta.setText(pushKey);
+        //Dialog.show("registeredForPush", "DeviceId: " + pushKey, "OK", "Cancel");
     }
     
     
@@ -100,5 +106,25 @@ public class Ashdod10 implements PushCallback{
         Dialog.show("ERROR", "registeredForPush():: Device Id is: " + error,
                 "OK", "Cancel");
     }
+    
+//    private void testPush(){
+//        ta = new TextArea();
+//        Form form = new Form();
+//        form.setLayout(new BorderLayout());
+//        form.add(BorderLayout.CENTER, ta);
+//        form.show();
+//
+//        Display.getInstance().scheduleBackgroundTask(new Runnable() {
+//            public void run() {
+//                try {
+//                    Hashtable meta = new Hashtable();
+//                    meta.put(com.codename1.push.Push.GOOGLE_PUSH_KEY, GOOGLE_MESSAGING_API_KEY);
+//                    Display.getInstance().registerPush(meta, false);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
 
 }
